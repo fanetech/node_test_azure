f0e2693052af395801575b7573bdfc35b6048a57
fanetech <louguefranck.20@gmail.com>
2023-06-12 18:13:13 +0000

    first commit

---
+++ api/index.js
@@ -0,0 +1,4 @@
+// const app =  require('./app');
+import app from '../server'
+
+export default app
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders:
+      "Origin, X-Requested-With, x-access-token, role, Content, Accept, Content-Type, Authorization",
+  })
+  
+  );
+  app.use("/api", (req, res) => {
+    console.log("it's working");
+    res.send('success received!') 
+  });
+  app.listen(3002, () => {
+    console.log('Server started!');
+    });
+
+export default app;
